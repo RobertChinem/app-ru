d7d869331a68eb245061b439884a4f4d3f47d351
robertchinem <robertchinem@gmail.com>
2022-10-04 18:13:52 -0300

    add page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,49 @@
 import axios from 'axios'
 import Cardapio from 'Cardapio'
-import { useEffect, useState } from 'react'
+import Home from 'icons/Home'
+import { ReactNode, useEffect, useState } from 'react'
 import Today from './Today'
+
+const routes: Array<{ icon: ReactNode; route: string }> = [
+  {
+    icon: <Home />,
+    route: 'all'
+  }
+]
+function Navbar() {
+  return (
+    <div className="grid h-full grid-flow-col gap-4 bg-gray-100 flex align-items-center">
+      {routes.map(({ route, icon }, index) => (
+        <div key={index}>
+          <button
+            className={`flex h-full w-full items-center justify-center p-4`}
+          >
+            {icon}
+          </button>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+function ListAllCardapios({ cardapios }: { cardapios: Cardapio[] }) {
+  return (
+    <div className="grid grid-cols-1 gap-4 px-4 pt-4">
+      {cardapios.map((cardapio, index) => (
+        <Today key={index} cardapio={cardapio} />
+      ))}
+    </div>
+  )
+}
+
+function Pages({ route, cardapios }: { route: string; cardapios: Cardapio[] }) {
+  switch (route) {
+    case 'all':
+      return <ListAllCardapios cardapios={cardapios} />
+    default:
+      return null
+  }
+}
 
 function App() {
   const [cardapios, setCardapios] = useState<Cardapio[]>([])
@@ -17,10 +59,13 @@ function App() {
   }, [])
 
   return (
-    <div className="grid grid-cols-1 gap-4">
-      {cardapios.map((cardapio, index) => (
-        <Today key={index} cardapio={cardapio} />
-      ))}
+    <div className="flex h-screen flex-col">
+      <div className="grow overflow-y-scroll">
+        <Pages route={'all'} cardapios={cardapios} />
+      </div>
+      <div className="h-12">
+        <Navbar />
+      </div>
     </div>
   )
 }

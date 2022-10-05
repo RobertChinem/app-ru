a9e360aeb8543c4f857b737846e50f70f353bd9e
robertchinem <robertchinem@gmail.com>
2022-10-04 21:16:34 -0300

    add icons

--- src/components/Today.tsx
+++ src/components/Today.tsx
@@ -1,39 +1,73 @@
 import Cardapio from 'Cardapio'
+import Moon from 'icons/Moon'
+import Sun from 'icons/Sun'
 
 function Today({ cardapio }: { cardapio: Cardapio }) {
   if (!cardapio) return null
 
   return (
-    <div className="rounded-xl border-2 border-gray-100 p-4 shadow-md">
+    <div className="rounded-xl border p-4">
       <h1 className="text-2xl">{cardapio.dia}</h1>
-      <div>
-        <h2 className="text-xl">Almoço</h2>
-        <div>
-          <h3 className="text-lg">Principal</h3>
-          {cardapio.almoco.principal}
+      <div className="mt-8 grid grid-cols-1 gap-4">
+        <div className="rounded-xl p-4 shadow-md">
+          <div className="mb-4 flex w-full items-center rounded-lg bg-white">
+            <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-amber-100 text-amber-500">
+              <Sun />
+            </div>
+            <div className="ml-3 text-sm font-bold">
+              <h1 className="text-xl">Almoço</h1>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 gap-2">
+            <div>
+              <h3 className="text-lg font-medium leading-4">Principal</h3>
+              <span className="text-sm font-light">
+                {cardapio.almoco.principal}
+              </span>
+            </div>
+            <div>
+              <h3 className="text-lg font-medium leading-4">Vegetariano</h3>
+              <span className="text-sm font-light">
+                {cardapio.almoco.vegetariano}
+              </span>
+            </div>
+            <div>
+              <h3 className="text-lg font-medium leading-4">Guarnição</h3>
+              <span className="text-sm font-light">
+                {cardapio.almoco.guarnicao}
+              </span>
+            </div>
+          </div>
         </div>
-        <div>
-          <h3 className="text-lg">Vegetariano</h3>
-          {cardapio.almoco.vegetariano}
-        </div>
-        <div>
-          <h3 className="text-lg">Guarnição</h3>
-          {cardapio.almoco.guarnicao}
-        </div>
-      </div>
-      <div>
-        <h2 className="text-xl">Jantar</h2>
-        <div>
-          <h3 className="text-lg">Principal</h3>
-          {cardapio.jantar.principal}
-        </div>
-        <div>
-          <h3 className="text-lg">Vegetariano</h3>
-          {cardapio.jantar.vegetariano}
-        </div>
-        <div>
-          <h3 className="text-lg">Guarnição</h3>
-          {cardapio.jantar.guarnicao}
+        <div className="rounded-xl p-4 shadow-md">
+          <div className="mb-4 flex w-full items-center rounded-lg bg-white">
+            <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-blue-200 text-blue-800">
+              <Moon />
+            </div>
+            <div className="ml-3 text-sm font-bold">
+              <h1 className="text-xl">Jantar</h1>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 gap-2">
+            <div>
+              <h3 className="text-lg font-medium leading-4">Principal</h3>
+              <span className="text-sm font-light">
+                {cardapio.jantar.principal}
+              </span>
+            </div>
+            <div>
+              <h3 className="text-lg font-medium leading-4">Vegetariano</h3>
+              <span className="text-sm font-light">
+                {cardapio.jantar.vegetariano}
+              </span>
+            </div>
+            <div>
+              <h3 className="text-lg font-medium leading-4">Guarnição</h3>
+              <span className="text-sm font-light">
+                {cardapio.jantar.guarnicao}
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
